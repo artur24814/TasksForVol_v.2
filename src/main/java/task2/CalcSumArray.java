ce6929ec3d355f479d9d9a39d33aa090e8133b84
Artur Yurchenko <artur24814@gmail.com>
2024-12-01 14:11:20 +0100

    The problem has been solved

--- src/main/java/task2/CalcSumArray.java
+++ src/main/java/task2/CalcSumArray.java
@@ -4,7 +4,7 @@ public class CalcSumArray {
     public static int getSum (int [] arr) {
         int result = 0;
 
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             result += arr[i];
         }
 
