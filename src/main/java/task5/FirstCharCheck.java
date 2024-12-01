488ee7d744ff1471ffc97bb2ed2fa843487bc5b8
Artur Yurchenko <artur24814@gmail.com>
2024-12-02 00:25:33 +0100

    Init unfinished solution

---
+++ src/main/java/task5/FirstCharCheck.java
@@ -0,0 +1,16 @@
+package task5;
+
+public class FirstCharCheck {
+    public static String repFirstChar(String text){
+        char firstChar = text.charAt(0);
+        if (...) {
+            return "upper";
+        } else if (...) {
+            return "lower";
+        } else if (...) {
+            return "digit";
+        } else {
+            return "other";
+        }
+    }
+}
