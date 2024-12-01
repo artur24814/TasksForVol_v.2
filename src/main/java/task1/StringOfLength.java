8d655bc0342082794312ac6d54daa93f27ef8647
Artur Yurchenko <artur24814@gmail.com>
2024-12-01 13:58:27 +0100

    Using StringBuilder instead of a simple string

--- src/main/java/task1/StringOfLength.java
+++ src/main/java/task1/StringOfLength.java
@@ -2,16 +2,19 @@ package task1;
 
 public class StringOfLength {
     public static String getLength (int number) {
-        // Execution time for n=1,000: 1008448200 nanoseconds
-        String result = "";
+        // Execution time for n=1,000: 1008448200 nanoseconds for using simple String
+        //String result = "";
+
+        // Execution time for n=1,000: 4781000 nanoseconds for StringBuilder
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < number; i++) {
             if (i % 2 == 0){
-                result = result + "+";
+                result.append("+");
             } else {
-                result = result + "-";
+                result.append("-");
             }
         }
-        return result;
+        return result.toString();
     }
 }
