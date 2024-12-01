841f2cc0f6fda6ad89f92c6ecdc63960d8c2e252
Artur Yurchenko <artur24814@gmail.com>
2024-12-01 15:09:58 +0100

    The second test case has been passed. The value entered should be between 1 and 100

--- src/main/java/task3/ZeroSumArray.java
+++ src/main/java/task3/ZeroSumArray.java
@@ -1,7 +1,13 @@
 package task3;
+import task3.NotOneTo100Exception;
 
 public class ZeroSumArray {
-    public static int[] generateArray(int expectedArrLength){
+    public static int[] generateArray(int expectedArrLength) throws NotOneTo100Exception {
+        if (expectedArrLength < 1 || expectedArrLength > 100){
+            throw new NotOneTo100Exception(
+                "expectedArrLength must be between 1 and 100 (inclusive)"
+            );
+        }
         return new int[] {0,0,0,0};
     }
 }
--- src/test/java/ZeroSumArrayTest.java
+++ src/test/java/ZeroSumArrayTest.java
@@ -1,13 +1,38 @@
 import org.junit.jupiter.api.Test;
 import task2.CalcSumArray;
 import task3.ZeroSumArray;
+import task3.NotOneTo100Exception;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ZeroSumArrayTest {
 
     @Test
-    void testIfSumOfReturningArrayIsZero () {
+    void testValidInputRange() {
+        assertDoesNotThrow(() -> CalcSumArray.getSum(ZeroSumArray.generateArray(1)));
+        assertDoesNotThrow(() -> CalcSumArray.getSum(ZeroSumArray.generateArray(100)));
+        assertDoesNotThrow(() -> CalcSumArray.getSum(ZeroSumArray.generateArray(50)));
+    }
+
+    @Test
+    void testInvalidInputRange() {
+        NotOneTo100Exception notOneTo100Exception1 = assertThrows(
+            NotOneTo100Exception.class,
+            () -> ZeroSumArray.generateArray(0),
+            "Expected exception for n=0, but it didn't occur"
+        );
+        assertEquals("expectedArrLength must be between 1 and 100 (inclusive)", notOneTo100Exception1.getMessage());
+
+        NotOneTo100Exception notOneTo100Exception2 = assertThrows(
+            NotOneTo100Exception.class,
+            () -> ZeroSumArray.generateArray(101),
+            "Expected exception for n=101, but it didn't occur"
+        );
+        assertEquals("expectedArrLength must be between 1 and 100 (inclusive)", notOneTo100Exception2.getMessage());
+    }
+
+    @Test
+    void testIfSumOfReturningArrayIsZero () throws NotOneTo100Exception {
         assertEquals(
             0,
             CalcSumArray.getSum(ZeroSumArray.generateArray(6)),
