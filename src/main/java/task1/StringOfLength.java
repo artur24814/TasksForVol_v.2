d82c7be5a2814ebcb07773579dc83fbb374899ff
Artur Yurchenko <artur24814@gmail.com>
2024-12-01 13:52:27 +0100

    Second test case passed, and test performance has been cheked

--- src/main/java/task1/StringOfLength.java
+++ src/main/java/task1/StringOfLength.java
@@ -2,10 +2,15 @@ package task1;
 
 public class StringOfLength {
     public static String getLength (int number) {
+        // Execution time for n=1,000: 1008448200 nanoseconds
         String result = "";
 
         for (int i = 0; i < number; i++) {
-            result = result + "=";
+            if (i % 2 == 0){
+                result = result + "+";
+            } else {
+                result = result + "-";
+            }
         }
         return result;
     }
